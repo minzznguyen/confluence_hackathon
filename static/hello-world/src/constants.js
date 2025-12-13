616e45f1bd6d8101b24ff73432b7659dcddf74af
voxuanthang203 <voxuanthang203@gmail.com>
2025-12-12 19:59:17 -0800

    chore: extract constants

--- static/hello-world/src/constants.js
+++ static/hello-world/src/constants.js
@@ -1,3 +1,7 @@
+// Forge App Configuration
+export const APP_ID = '24e34540-92df-4b82-81a2-46340f7d3440';
+export const DEVELOPMENT_ENV = '839b6a23-c454-41b1-9b1e-9468893d5204';
+
 export const API_ENDPOINTS = {
   PAGE: (pageId) => `/wiki/api/v2/pages/${pageId}?body-format=storage`,
   INLINE_COMMENTS: (pageId) =>
--- static/hello-world/src/utils/navigation.js
+++ static/hello-world/src/utils/navigation.js
@@ -1,4 +1,5 @@
 import { router } from "@forge/bridge";
+import { APP_ID, DEVELOPMENT_ENV } from "../constants";
 
 // Navigates from byline item to full page analytics view
 export async function navigateToFullPage(context) {
@@ -10,8 +11,8 @@ export async function navigateToFullPage(context) {
   const appIdMatch = currentPath.match(/\/forge-apps\/a\/([^\/]+)/);
   const envIdMatch = currentPath.match(/\/e\/([^\/]+)/);
   
-  const appId = appIdMatch?.[1] || '24e34540-92df-4b82-81a2-46340f7d3440';
-  const envId = envIdMatch?.[1] || '839b6a23-c454-41b1-9b1e-9468893d5204';
+  const appId = appIdMatch?.[1] || APP_ID;
+  const envId = envIdMatch?.[1] || DEVELOPMENT_ENV;
 
   const params = pageId ? `?pageId=${pageId}` : '';
   router.open(`/forge-apps/a/${appId}/e/${envId}/r/hello-world${params}`);
