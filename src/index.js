9d7eec2251f4963f51acc84927c335b5f4821690
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2025-12-20 20:54:46 -0800

    This version is working

--- src/index.js
+++ src/index.js
@@ -20,8 +20,8 @@ export function getConfluenceComments(message) {
   // const comments = getInlineComments("1900549");
   // console.log("Thao testing comments in getConfluenceComments:", comments);
   // return comments;
-  console.log("Thao testing getConfluenceComments???");
-  return [
+  console.log("Thao testing getConfluenceComments!");
+  const comments = [
     {
       commentId: "1",
       content: "Thao testing 1",
@@ -33,22 +33,30 @@ export function getConfluenceComments(message) {
       replies: 0,
     },
   ];
+
+  return JSON.stringify(comments);
 }
 
 export function countReplies(comments) {
+  console.log("Thao testing comments:", comments);
+  console.log("Thao testing comments:", comments.comments);
+
   // const ranked = rankParentsByReplies(comments, {
   //   status: COMMENT_STATUS.OPEN,
   // });
   // const topComment = ranked.slice(0, 1)[0];
   // const text = getCommentLabel(topComment, 100);
   // return text;
-  let topComment = comments[0];
+  // Find the comment with the most replies
+  const parsedComments = JSON.parse(comments.comments);
+  let topComment = parsedComments[0];
 
-  for (const comment of comments) {
+  for (const comment of parsedComments) {
     if (comment.replies > topComment.replies) {
       topComment = comment;
     }
   }
 
+  // Return only the content
   return topComment.content;
 }
